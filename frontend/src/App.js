651b233340b1420e2cee9e5a20ea7317f8b69acd
Colby McCarty <ctmc.enterprises@gmail.com>
2024-11-16 08:14:44 -0700

    baseline app render  fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,41 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Home from './pages/Home';
+// import Login from './pages/Login';
+// import Register from './pages/Register';
 // import other components...
 
+// const App = () => (
+//   <Router>
+//     <Routes>
+//       <Route path="/login" component={Login} />
+//       <Route path="/register" component={Register} />
+//       <Route path="/" component={Home} />
+//       {/* Define other routes... */}
+//     </Routes>
+//   </Router>
+// );
+
+// export default App;
+
+import React from 'react';
+import Home from './pages/Home';
+
 const App = () => (
-  <Router>
-    <Switch>
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-      <Route path="/" component={Home} />
-      {/* Define other routes... */}
-    </Switch>
-  </Router>
+  <div>
+    <Home />
+  </div>
 );
 
 export default App;
+
+
+// import React from 'react';
+
+// const App = () => (
+//   <div>
+//     <h1>App is Rendering</h1>
+//   </div>
+// );
+
+// export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/styles.css'; // Ensure your styles load here (optional)
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
