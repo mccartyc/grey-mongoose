a995ebaa0819935a0ad4e7b410abbf54e16eebf7
Colby McCarty <ctmc.enterprises@gmail.com>
2024-11-22 07:35:48 -0700

    updated backend and frontend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 // File: backend/server.js
-
 const express = require('express');
-const { connectDB } = require('./config/db'); // Import the database connection
+const connectDB = require('./config/db'); // Import the database connection
+const tenantRoutes = require('./routes/tenantRoutes');
+const clientRoutes = require('./routes/clientRoutes');
+const userRoutes = require('./routes/userRoutes');
+const cors = require('cors');
 require('dotenv').config(); // Load .env variables
 
 const app = express();
+
+// Enable CORS
+app.use(cors());
+
 const PORT = process.env.PORT || 5001;
 
 // Middleware
@@ -14,7 +21,10 @@ app.use(express.json());
 connectDB();
 
 // Routes
-app.use('/api/users', require('./routes/userRoutes'));
+// app.use('/api/users', require('./routes/authRoutes'));
+app.use('/api/tenants', tenantRoutes);
+app.use('/api/clients', clientRoutes);
+app.use('/api/users', userRoutes);
 
 // Start the server
 app.listen(PORT, () => {
