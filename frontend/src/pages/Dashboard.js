447969c813e610e18b952278e88c799bf8641818
mccartyc <ctmc.enterprises@gmail.com>
2025-01-09 16:07:54 -0700

    Dashboard Creation

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,18 +1,19 @@
 // src/pages/Dashboard.js
 import React from 'react';
 import SideNavBar from '../components/SideNavBar';
+import Dashboard from '../components/Dashboard';
 import '../styles/styles.css';
 
-const Dashboard = () => {
+const DashboardPage = () => {
   return (
       <div className="main-content">
         <SideNavBar />
         <div className="content-area">
           <h1>Dashboard</h1>
-          <p>Welcome to your Dashboard! Use the side navigation to access different sections.</p>
+          <Dashboard />
         </div>
       </div>
   );
 };
 
-export default Dashboard;
+export default DashboardPage;
