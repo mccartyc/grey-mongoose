8cf42f58e25311091b07d3c4dc84219676022dd3
Colby McCarty <ctmc.enterprises@gmail.com>
2025-01-12 14:57:09 -0700

    working to update ids

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -6,21 +6,16 @@ const userSchema = new mongoose.Schema({
   firstname: { type: String, required: true },
   lastname: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  userId: { type: String, default: uuidv4, unique: true },
+  // userId: { type: String, default: uuidv4, unique: true },
   password: { type: String, required: true },
   role: { type: String, enum: ["Admin", "User"], default: "User" }, // User roles
-  tenantId: {type: String, required: true }, // Reference to Tenant
+  // tenantId: {type: String, required: true }, // Reference to Tenant
+  tenantId: { type: mongoose.Schema.Types.ObjectId, ref: 'Tenant', required: true }, // Reference to Tenant
   refreshToken: { type: String }, // Store the latest refresh token
   createdAt: { type: Date, default: Date.now },
   isActive: { type: Boolean, required: true, default: true } // Field to indicate if user is active
 });
 
-// // Encrypt password before saving
-// userSchema.pre("save", async function (next) {
-//   if (!this.isModified("password")) return next();
-//   this.password = await bcrypt.hash(this.password, 10);
-//   next();
-// });
 
 // Method to compare passwords
 userSchema.methods.matchPassword = async function (enteredPassword) {
