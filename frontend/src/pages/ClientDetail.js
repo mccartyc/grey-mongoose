d112d5261ff419727f0a3bbff04c1b5c45072a83
Colby McCarty <ctmc.enterprises@gmail.com>
2025-01-15 20:36:30 -0700

    commiting changes

--- frontend/src/pages/ClientDetail.js
+++ frontend/src/pages/ClientDetail.js
@@ -3,13 +3,18 @@ import React from 'react';
 import SideNavBar from '../components/SideNavBar';
 import '../styles/styles.css';
 import ClientDetail from '../components/ClientDetail.js';
+import ClientNavBar from '../components/ClientNavBar.js';
 
 const ClientList = () => {
   return (
       <div className="main-content">
         <SideNavBar />
         <div className="content-area">
-          <ClientDetail />
+          <div className="client-detail-header-container">
+            <h1>Client Detail</h1>
+            <ClientNavBar/>
+          </div>
+            <ClientDetail />
         </div>
       </div>
   );
--- frontend/src/pages/Intake.js
+++ frontend/src/pages/Intake.js
@@ -3,12 +3,17 @@ import React from 'react';
 import SideNavBar from '../components/SideNavBar';
 import '../styles/styles.css';
 import IntakeForm from '../components/IntakeForm.js';
+import ClientNavBar from '../components/ClientNavBar.js';
 
 const IntakeFormPage = () => {
   return (
       <div className="main-content">
         <SideNavBar />
         <div className="content-area">
+        <div className="client-detail-header-container">
+            <h1>Client Detail</h1>
+            <ClientNavBar/>
+            </div>
           <IntakeForm />
         </div>
       </div>
